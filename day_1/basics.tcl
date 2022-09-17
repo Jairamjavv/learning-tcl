1c79742fe805ffa9ffb51e6d9fcb9c52d4fce7db
JairamB <b.jairam0512@gmail.com>
2022-09-17 16:31:26 +0530

    Getting started

---
+++ day_1/basics.tcl
@@ -0,0 +1,12 @@
+;# Hello world. ;# is used to write comments in TCL Language.
+puts "TCL: Tool Command Language."
+
+;# working with variables. 
+;# datatype not required
+set name "Jairam"
+puts "The name is $name" ;# you use $ to address a variable.
+
+set birthyear 1997
+puts "$name was born in $birthyear"
+
+;# nested command substitution 
